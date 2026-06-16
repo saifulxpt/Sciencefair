7c163681a1b0c4a92550fe259a515f676240394a
Saiful Islam <saiful@example.com>
2026-06-16 17:25:05 +0600

    Add fallback to prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DIRECT_URL"],
+    url: process.env["DIRECT_URL"] || "postgresql://dummy:dummy@dummy:5432/dummy",
   },
 });
